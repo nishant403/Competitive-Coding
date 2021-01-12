a0c1e27c5684a3140fa6bd17d21452a8370a8ec2
nishant403 <43030867+nishant403@users.noreply.github.com>
2021-01-13 00:35:01 +0530

    Update HLD_template.cpp

--- My codes/HLD_template.cpp
+++ My codes/HLD_template.cpp
@@ -1,8 +1,7 @@
-//https://codeforces.com/gym/100551/problem/C
-
 #include <bits/stdc++.h>
 using namespace std;
  
+#define int long long 
 #define pb push_back
 #define S second
 #define F first
@@ -11,44 +10,38 @@ using namespace std;
 #define vi vector<int>
 #define pii pair<int,int>
 
-const int N = 1e5+10;
-const int LN = 17;
+const int N = 2e5+10;
+const int LN = 18;
 
 //SEGTREE
 int seg[4*N];
-int lazy[4*N];
 
-void push(int node,int ss,int se)
+void update(int node,int ss,int se,int id,int val)
 {
-    if(lazy[node] == 0) return;
+    if(se < id || id < ss) return;
     
-    if(lazy[node] == 2) seg[node] = se - ss + 1;
-    else seg[node] = 0;
-    
-    if(ss != se)
-        lazy[node*2+1] = lazy[node*2+2] = lazy[node];
-    
-    lazy[node] = 0;
-}
-
-void update(int node,int ss,int se,int l,int r,int val)
-{
-    push(node,ss,se);
-    
-    if(se < l || r < ss) return;
-    
-    if(l <= ss && se <= r)
+    if(id <= ss && se <= id)
     {
-        lazy[node] = val + 1;
-        push(node,ss,se);
+        seg[node] = val;
         return;
     }
     
     int mid = (ss + se)/2;
     
-    update(node*2+1,ss,mid,l,r,val);
-    update(node*2+2,mid+1,se,l,r,val);
-    seg[node] = seg[node*2+1] + seg[node*2+2];
+    update(node*2+1,ss,mid,id,val);
+    update(node*2+2,mid+1,se,id,val);
+    seg[node] = max(seg[node*2+1] , seg[node*2+2]);
+}
+
+int query(int node,int ss,int se,int l,int r)
+{
+    if(se < l || r < ss) return 0;
+    
+    if(l <= ss && se <= r) return seg[node];
+    
+    int mid = (ss + se)/2;
+    
+    return max(query(node*2+1,ss,mid,l,r) , query(node*2+2,mid+1,se,l,r));
 }
 
 vi g[N];
@@ -58,6 +51,7 @@ int sz[N];
 int heavy[N];
 int head[N];
 int pos[N];
+int a[N];
 int cur_pos = 0;
 int n;
 
@@ -67,6 +61,7 @@ void dfs(int node,int p = -1)
     else dep[node] = dep[p] + 1;
     
     par[node] = p;
+    
     sz[node] = 1;
     
     for (int x : g[node]) 
@@ -81,29 +76,35 @@ void dfs2(int v, int hed)
 {
     pos[v] = cur_pos++;
     head[v] = hed;
-    
+    update(0,0,n-1,pos[v],a[v]);
     int mx = -1, bigChild = -1;
     
     for(auto u : g[v])
-       if(u != par[u] && sz[u] > mx)
+    {
+       if(u != par[v] && sz[u] > mx)
              mx = sz[u], bigChild = u;
+    }
     
     if(bigChild != -1) dfs2(bigChild,hed);
     
     for(auto u : g[v])
-        if(u != par[u] && u != bigChild) dfs2(u,u);  
+        if(u != par[v] && u != bigChild) dfs2(u,u);  
 }
  
-void paint(int a, int b) 
+int paint(int a, int b) 
 {
+    int res = 0;
+    
     for (; head[a] != head[b]; b = par[head[b]])
     {
         if(dep[head[a]] > dep[head[b]]) swap(a,b); 
-        update(0,0,n-1,pos[head[b]],pos[b],1);
+        res = max(res , query(0,0,n-1,pos[head[b]],pos[b]));
     }
     
     if (dep[a] > dep[b]) swap(a, b);
-    update(0,0,n-1,pos[a]+1,pos[b],1);
+    res = max(res,query(0,0,n-1,pos[a],pos[b]));
+    
+    return res;
 }
 
 void solve()
@@ -111,10 +112,15 @@ void solve()
   int q,x,l,r;
   cin >> n >> q;
     
+  for(int i=1;i<=n;i++) cin >> a[i];
+    
+  int u,v;
+    
   for(int i=2;i<=n;i++)
   {
-    cin >> x;
-    g[x].pb(i);
+    cin >> u >> v;
+    g[u].pb(v);
+    g[v].pb(u);
   }
     
   dfs(1); 
@@ -122,25 +128,17 @@ void solve()
     
   while(q--)
   {
-     int k;
-     cin >> k;
-      
-     while(k--)
-     {
-        cin >> l >> r;
-        paint(l,r);
-     }
+      int t,l,r;
+      cin >> t >> l >> r;
      
-     cout << n - 1 - seg[0] << '\n'; 
-  
-     update(0,0,n-1,0,n-1,0); 
+      if(t == 1) update(0,0,n-1,pos[l],r);
+      else cout << paint(l,r) << ' ';
   }
     
 }
  
 signed main()
 {
-    freopen("bridges2.in","r",stdin);
-    freopen("bridges2.out","w",stdout);
+    fast;
     solve();
 }
