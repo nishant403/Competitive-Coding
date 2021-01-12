71d3a5e1be63c0958edd057e6a83586585f20db5
nishant403 <43030867+nishant403@users.noreply.github.com>
2021-01-12 19:02:38 +0530

    Update NCR.cpp

--- My codes/NCR.cpp
+++ My codes/NCR.cpp
@@ -1,36 +1,19 @@
-const ll mod = 1e9+7;
-const ll M = 1e9+7;
-const ll N = 1e6+5;
-vector<ll> fact(N) , ifact(N);
 
+const int N = 1e5 + 10;
 
-int inv(int x)
-{
- int res = 1;
- int b = M - 2;
-    
-  while(b)
-  {
-    if(b&1) res=(res*x)%M;
-    x=(x*x)%M;
-    b>>=1;
-  }
-    
-    return res;
-}
+int fact[N];
+int ifact[N];
 
 void init()
 {
     fact[0] = 1;
     
-    for(int i=1;i<N;i++) fact[i] = (fact[i-1]*i)%M;
+    for(int i=1;i<N;i++) fact[i] = (fact[i-1]*i)%MOD;
     
-    ifact[(int)1e6] = inv(fact[(int)1e6]);
+    ifact[N-1] = mod_pow(fact[N-1],MOD-2);
     
-    for(int i=(int)1e6-1;i>=0;i--) 
-        ifact[i] = (ifact[i+1]*(i+1))%M;
-    
-    
+    for(int i=N-2;i>=0;i--) 
+        ifact[i] = (ifact[i+1]*(i+1))%MOD;
 }
 
 int ncr(int n,int r)
@@ -39,8 +22,10 @@ int ncr(int n,int r)
     
  int res = fact[n];
     
- res=(res*ifact[r])%M;
- res=(res*ifact[n-r])%M;
+ res = (res*ifact[r])%MOD;
+ res = (res*ifact[n-r])%MOD;
     
   return res;
 }
+
+
