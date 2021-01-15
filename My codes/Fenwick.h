03deb3556ed8ddc2245a39420d2c41aac5fffe42
nishant403 <43030867+nishant403@users.noreply.github.com>
2021-01-15 19:11:14 +0530

    Update Fenwick.h

--- My codes/Fenwick.h
+++ My codes/Fenwick.h
@@ -1,30 +1,30 @@
-struct Fenwick                        //Xor-Fenwick
-{
-    ll jt[N];
-    void clear()
-    {
-        memset( jt , 0 , sizeof(jt));
+struct FenwickTree {
+    vector<int> bit;  // binary indexed tree
+    int n;
+
+    FenwickTree(int n) {
+        this->n = n;
+        bit.assign(n, 0);
     }
-    void set( ll p , ll v )
-    {
-        while( p < N )
-        {
-            jt[p] ^= v;
-            p += (p & (-p));
-        }
+
+    FenwickTree(vector<int> a) : FenwickTree(a.size()) {
+        for (size_t i = 0; i < a.size(); i++)
+            add(i, a[i]);
     }
-    ll query( ll p )
-    {
-        ll temp = 0;
-        while( p > 0 )
-        {
-            temp ^= jt[p];
-            p -= (p & (-p));
-        }
-        return temp;
+
+    int sum(int r) {
+        int ret = 0;
+        for (; r >= 0; r = (r & (r + 1)) - 1)
+            ret += bit[r];
+        return ret;
     }
-    ll range_query( ll l , ll r)
-    {
-        return query(l) ^ query(r);
+
+    int sum(int l, int r) {
+        return sum(r) - sum(l - 1);
     }
-}
+
+    void add(int idx, int delta) {
+        for (; idx < n; idx = idx | (idx + 1))
+            bit[idx] += delta;
+    }
+};
